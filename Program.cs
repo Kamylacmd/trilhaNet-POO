fc17fff63fc9e18faad5aff35e4e84e80c4fe57a
Kamylacmd <kamylavitoria24@gmail.com>
2025-11-08 23:38:44 -0300

    Projeto feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("NOKIA:");
+Smartphone nokia = new Nokia("123456", "modelo 2", "121212", 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n-------------------\n");
+
+Console.WriteLine("IPHONE:");
+Smartphone iphone = new Iphone("654321", "modelo 1", "343434", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Nubank");
